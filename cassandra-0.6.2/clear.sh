a4dedc366c24731988cfa2e237ba7b7f938eb8a2
sunsuk7tp <sunsuk7tp@gmail.com>
2011-05-15 02:13:17 +0900

    del org.

--- cassandra-0.6.2/clear.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm /work1/nakamura/cassandra/log/system.log
-rm -r /work1/nakamura/cassandra/data/*
-rm -r /work1/nakamura/cassandra/commitlog/*
--- cassandra-0.6.2/interface/avro/gen-java/org/apache/cassandra/avro/ConsistencyLevel.java
+++
@@ -1,6 +0,0 @@
-package org.apache.cassandra.avro;
-
-@SuppressWarnings("all")
-public enum ConsistencyLevel { 
-  ZERO, ONE, QUORUM, DCQUORUM, DCQUORUMSYNC, ALL
-}
