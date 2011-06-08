f0ef24a2d7f56223cf898ae198a8b27186a944c4
sunsuk7tp <sunsuk7tp@gmail.com>
2011-06-08 22:38:44 +0900

    add copyright

--- MyCassandra-0.7.5/src/java/org/apache/cassandra/db/engine/RangeMySQLInstance.java
+++ MyCassandra-0.7.5/src/java/org/apache/cassandra/db/engine/RangeMySQLInstance.java
@@ -1,3 +1,19 @@
+/*                                                                                                                                                                                 
+ * Copyright 2011 Shunsuke Nakamura, and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.cassandra.db.engine;
 
 import java.sql.*;
@@ -68,7 +84,6 @@ public class RangeMySQLInstance extends RangeDBInstance
 	    }
 	}
 
-	//overide
     private void setStatementDefinition()
     {
         /* define CRUD statements */
@@ -138,9 +153,7 @@ public class RangeMySQLInstance extends RangeDBInstance
             ResultSet rs = pstRange.executeQuery();
             if (rs != null)
                 while (rs.next())
-                {
                     rowMap.put(ByteBuffer.wrap(rs.getBytes(1)), bytes2ColumnFamily(rs.getBytes(2)));
-                }
             rs.close();
             pstRange.close();
             return rowMap;
