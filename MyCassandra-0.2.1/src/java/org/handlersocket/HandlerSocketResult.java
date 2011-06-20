5fb407df3bba39c4902935db16028bbae6a4e23c
sunsuk7tp <sunsuk7tp@gmail.com>
2011-06-20 22:40:20 +0900

    format bug fix

--- MyCassandra-0.2.1/src/java/org/handlersocket/HandlerSocketResult.java
+++ MyCassandra-0.2.1/src/java/org/handlersocket/HandlerSocketResult.java
@@ -9,52 +9,53 @@ import java.util.List;
  *
  */
 public class HandlerSocketResult {
-	private int status;//ステータスコード
-	private int fieldNum;//返却されたメッセージの数
-	private byte[] messages;//メッセージ
+    private int status;//ステータスコード
+    private int fieldNum;//返却されたメッセージの数
+    private List<byte[]> messages;//メッセージ
 
-	public HandlerSocketResult(){
-		super();
-	}
-	
-	public HandlerSocketResult(int status, int fieldNum, byte[] messages){
-		this();
-		this.status = status;
-		this.fieldNum = fieldNum;
-		this.messages = messages;
-	}
+    public HandlerSocketResult(){
+        super();
+    }
+    
+    public HandlerSocketResult(int status, int fieldNum, List<byte[]> messages){
+        this();
+        this.status = status;
+        this.fieldNum = fieldNum;
+        this.messages = messages;
+    }
 
-	public int getStatus() {
-		return status;
-	}
+    public int getStatus() {
+        return status;
+    }
 
-	public void setStatus(int status) {
-		this.status = status;
-	}
+    public void setStatus(int status) {
+        this.status = status;
+    }
 
-	public int getFieldNum() {
-		return fieldNum;
-	}
+    public int getFieldNum() {
+        return fieldNum;
+    }
 
-	public void setFieldNum(int fieldNum) {
-		this.fieldNum = fieldNum;
-	}
+    public void setFieldNum(int fieldNum) {
+        this.fieldNum = fieldNum;
+    }
 
-	public byte[] getMessages() {
-		return messages;
-	}
+    public List<byte[]> getMessages() {
+        return messages;
+    }
 
-	public void setMessages(byte[] messages) {
-		this.messages = messages;
-	}
-	
-	@Override
-	public String toString(){
-		StringBuilder builder = new StringBuilder();
-		builder.append(status).append("\t");
-		builder.append(fieldNum).append("\t");
-		builder.append(new String(messages)).append("\t");
-		
-		return builder.toString();
-	}
+    public void setMessages(List<byte[]> messages) {
+        this.messages = messages;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        builder.append(status).append("\t");
+        builder.append(fieldNum).append("\t");
+        for (byte[] message : messages) 
+            builder.append(new String(message)).append("\t");
+        
+        return builder.toString();
+    }
 }
